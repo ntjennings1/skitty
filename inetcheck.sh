57130eba443b2aab7a6d66f2abcd63ff2d69fc4c
ntjennings1 <ntjennings1@gmail.com>
2026-02-19 12:37:57 -0500

    Script Logo Standard 0.3

--- inetcheck.sh
+++ inetcheck.sh
@@ -4,15 +4,44 @@ STATUS=1
 
 function throw_exec(){
   if [ "$1" == "con" ]; then
+    clear
+    logo
     STATUS=0
-    echo "[!] Error connecting to the Internet."
+    describe "No web access."
   fi
+}
+
+function logo(){
+cat<<'EOF'
+|---------------------------|
+|          ,  -  .          |
+|        0 ------- 0        |
+|     0               0     |
+|    0 --------------- 0    |
+|   (                   )   |
+|    0 --------------- 0    |
+|     0               0     |
+|        0 ------- 0        |
+|           * _ *           |
+|---------------------------|
+EOF
+}
+
+function describe(){
+
+  echo
+  echo '|---------------------------|'
+  echo "  $1"
+  echo '|---------------------------|'
+
 }
 
 function inetcheck(){
   ping google.com -c 1 &> /dev/null || throw_exec "con"
   if [ $STATUS -eq 1  ]; then
-    echo 'Connected.'
+    clear
+    logo
+    describe 'Connected to the Internet.'
   fi
 }
 
